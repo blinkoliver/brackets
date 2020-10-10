a70c33275a5c6217f7814313c52839f6bb05c05d
Anton Barouka <52159126+blinkoliver@users.noreply.github.com>
2020-10-10 13:39:39 +0300

    init: start and end brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = check = (str, bracketsConfig) => {
+    const pairs = bracketsConfig.map((element) => element.join("")).flat(); 
+    for (let i = 0; i < pairs.length; i++) {
+        if (str.includes(pairs[i])) {
+            str = str.replace(pairs[i], "");
+            i = -1;
+        }
+    }
+    return str.length === 0;
+};
